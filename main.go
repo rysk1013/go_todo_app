018ea179e64bd1f600daa90b46b6afc29cb386c3
rysk1013 <rysk2908@gmail.com>
2021-06-05 21:51:17 +0900

    display index.html

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
-	_ "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.html")
+
+	data := "Hello Go/Gin"
+
+	router.GET("/", func (ctx *gin.Context)  {
+		ctx.HTML(200, "index.html", gin.H{"data": data})
+	})
+
+	router.Run() // localhost:8080
 }
